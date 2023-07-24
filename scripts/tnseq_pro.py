df656534df8edc8bbb282da3678f12598b42138b
Jennifer Stiens <37145031+jenjane118@users.noreply.github.com>
2023-07-24 17:59:17 +0100

    strand-specific ta-site identification

--- scripts/tnseq_pro.py
+++ scripts/tnseq_pro.py
@@ -287,6 +287,24 @@ def open_fasta(refseq):
             if line[0] != '>':
                 seq += line.strip()
     return seq
+
+#**********************************************************************************
+
+def filter_reads_by_strand(reads):
+    """
+    Function to filter reads by strand
+    Input               reads           list of reads
+    Output              fwd_strand_reads    list of reads mapped to forward strand
+                        rev_strand_reads    list of reads mapped to reverse strand
+    """
+    fwd_strand_reads = []
+    rev_strand_reads = []
+    for read in reads:
+        if parse_samflag(read) == "F":
+            fwd_strand_reads.append(read)
+        else:
+            rev_strand_reads.append(read)
+    return fwd_strand_reads, rev_strand_reads
     
 #**********************************************************************************
 
@@ -294,21 +312,69 @@ def find_ta_position(read, tag_pos):
     """
     Find the TA position relative to + strand
     Input           read                        sequencing read (mapped) from sam file
+                    tag_pos                     position of tag in read (from find_tags3)
+    Output          ta_position                 predicted position of insertion site in read
+                                                (end of tag)
 
 
     """
-    strand = parse_samflag(read)
-    left_pos = read.split()[3]
+    
+    strand      = parse_samflag(read)
+    left_pos    = read.split()[3]
     if tag_pos != -1:
         if strand == 'F':
-            #TA should be the left most position == soft-clipped bases + tag_pos
-            ta_position = int(left_pos)
-        else:
+            ta_position = int(left_pos) #(this is a few bases off when sequence matches end of transposon tag)
+        else: #reverse strand
             ta_position = int(left_pos) + tag_pos
     else:
         ta_position = "-"
     return ta_position
 
+#**********************************************************************************
+
+def find_tags_fwd(read, target_tag, max):
+    """
+    Find transposon tag in sequence of each forward read.
+
+      Input           read              mapped read to forward strand (+)
+                      target_tag        string that matches transposon sequence
+                      max               maximum number of mismatches allowed (default=2)
+      Output          start             calculation of start of read/insertion point 
+                                        from left-most start position of tag or -1 if no match
+    """
+    
+    seq = read.split()[9]
+    
+    #search string for transposon seq with max num mismatches
+    match = mmfind1(seq, len(seq), target_tag, len(target_tag), max)
+    if match != -1:
+        start = match + len(target_tag) #this gives start position of read (start of ta site)
+    else:
+        start = match
+    
+    return start
+
+#**********************************************************************************
+
+def find_tags_rev(read, target_tag, max):
+
+    from Bio.Seq import Seq
+    #find rc of tag
+    tag_seq = Seq(target_tag)
+    rc_tag  = str(tag_seq.reverse_complement())
+    seq = read.split()[9]
+    ##strand is reverse start searching sequence from other end 
+    #search reverse sequence (3' to 5') for rc of tag (starting with TAAC--) start position of tag is end of tag and start of insert/ta site
+    #in some cases, soft-clipped bases at 3' end of read will make tag too long
+    cig = read.split()[5]
+    cig_list = parse_cigar(cig)
+    if cig_list[0][-1]=="S": #if there is soft-clipping at 3' end of reverse read
+        sc_correction = int(cig_list[0][:-1]) #number of soft-clipped bases
+        match = mmfind1(seq[sc_correction:len(seq)], len(seq), rc_tag, len(rc_tag), max) #start search from start of aligned bases
+    else:
+        match = mmfind1(seq, len(seq), rc_tag, len(rc_tag), max)
+    
+    return match
 
 #**********************************************************************************
 
@@ -377,6 +443,96 @@ def take_closest(myList, myNumber):
     
 #**********************************************************************************
 
+def filter_mapped_reads_tag3(sam_file, tag="ACTTATCAGCCAACCTGTTA", mismatch_max=2):
+    """ 
+    Revised filtering function: filters reads for presence of transposon tag and for duplicate barcode/ta-position
+
+    Input               sam_file                            .sam file of mapped reads
+                        tag                                 str of transposon tag used, default is Himar1
+                        mismatch_max                        integer of maximum mismatches allowed in search for tag
+    Output              read barcodes                       list of 3 values: insert_start, strand, barcode from reads with insertions
+                                                        
+    """
+    import sys
+    import os
+
+    #check tag has valid nucleotides
+    if check_seq(tag):
+        pass
+    else:
+        sys.exit("Invalid sequence tag")
+
+    #read sam_file and sort lines between header and reads
+    data    = read_samfile(sam_file)
+    header  = data[0]
+    reads   = data[1]
+
+    #divide reads by strand
+    fwd_strand_reads, rev_strand_reads = filter_reads_by_strand(reads)
+    
+    tagged_reads    = []
+    barcode_list_fwd    = []
+    barcode_list_rev    = []
+    barcode_list_all    = []
+    notag_count = 0
+
+    for read in fwd_strand_reads:
+        tag_pos   = find_tags_fwd(read, tag, mismatch_max)
+        strand    = "F"
+        if tag_pos != -1:  #there is match for tranposon tag
+            insertion_coord = find_ta_position(read, tag_pos)
+            #find barcode
+            read_name  = read.split()[0]
+            barcode    = read_name.split("BC:",1)[1]
+            pos_barcode = [insertion_coord, strand, barcode]
+            #add to list of all reads with insertions
+            barcode_list_fwd.append(pos_barcode) 
+            #append read to tagged reads
+            tagged_reads.append(read)
+        else:
+            notag_count += 1
+  
+    print("Total number of mapped reads forward strand: ", len(fwd_strand_reads))
+    print("number of forward reads with tag: ", len(barcode_list_fwd))
+    print("number of forward reads with no tag: ", notag_count)
+  
+    
+    notag_count = 0
+
+    for read in rev_strand_reads:
+        tag_pos   = find_tags_rev(read, tag, mismatch_max)
+        strand    = "R"
+        if tag_pos != -1:  #there is match for tranposon tag
+            insertion_coord = find_ta_position(read, tag_pos)
+            #find barcode
+            read_name  = read.split()[0]
+            barcode    = read_name.split("BC:",1)[1]
+            pos_barcode = [insertion_coord, strand, barcode]
+            #add to list of all reads with insertions
+            barcode_list_rev.append(pos_barcode) 
+            #append read to tagged reads
+            tagged_reads.append(read)
+        else:
+            notag_count += 1
+  
+    print("Total number of mapped reads reverse strand: ", len(rev_strand_reads))
+    print("number of forward reads with tag: ", len(barcode_list_rev))
+    print("number of forward reads with no tag: ", notag_count)
+
+    #write read files (includes all fwd and reverse reads)
+    bn = os.path.basename(sam_file)
+    outfile = "tag_filtered_templates_" + bn
+    with open(outfile, 'w') as f:
+        for line in header:
+            f.write(f"{line}\n")
+        for line in tagged_reads:
+            f.write(f"{line}\n")
+
+    #barcode_list_all = barcode_list_fwd + barcode_list_rev
+    return barcode_list_fwd, barcode_list_rev
+
+#**********************************************************************************
+
 #function to go through all possible ta sites and create .wig file of insertions
 
 def ta_sites_to_dict(ta_sites, read_count_dict):
@@ -417,19 +573,37 @@ def write_wig_from_dict(wig_dict, sample_name, genome):
 
 #**********************************************************************************
 
-def assign_counts_to_sites(ta_sites, barcode_position_list):
+def assign_counts_to_sites(ta_sites, barcode_position_lists):
     """
     Function to tally ta sites with insertions
+    Input               ta_sites                ordered list of all possible ta sites in Mbovis genome
+                        barcode_position_list   list of lists of barcode, strand, and insertion position for each read             
+                        strand                  strand of reads in barcode list (F or R)
+
     """
 
-    insertions_list = [line[0] for line in barcode_position_list]
+    
     read_count_dict = {}
     no_match        = []
 
+    fwd_insertions = barcode_position_lists[0]
+    insertions_list = [line[0] for line in fwd_insertions]
     for site in insertions_list:
-        #check if ins_site is in list of ta sites (or within 1 nt of ta position)
+        #check if ins_site is in list of ta sites (or within 3 nt of ta position)
         closest_ta = take_closest(ta_sites, site)
-        if abs(closest_ta - site) < 2:
+        if site - closest_ta < 4:
+            if closest_ta not in read_count_dict:
+                read_count_dict[closest_ta] = 1
+            else:
+                read_count_dict[closest_ta] += 1
+        else:
+            no_match.append(site)
+
+    rev_insertions = barcode_position_lists[1]
+    insertions_list = [line[0] for line in rev_insertions]
+    for site in insertions_list:
+        closest_ta = take_closest(ta_sites, site)
+        if closest_ta - site < 4:  #if alignment includes up to 3 bases of transposon seq will be smaller number than actual ta coord
             if closest_ta not in read_count_dict:
                 read_count_dict[closest_ta] = 1
             else:
@@ -456,9 +630,12 @@ def sam_to_wig(samfile, genome_fasta, sample_name):
     fasta_seq   = open_fasta(genome_fasta)
     ta_sites    = find_insertion_sites(fasta_seq)
     # make list of the positions of every insertion from unique templates
-    template_positions = filter_mapped_reads3(samfile)
+    template_positions = filter_mapped_reads_tag3(samfile, tag="ACTTATCAGCCAACCTGTTA", mismatch_max=2)
     #count number of reads per ta site
     res=assign_counts_to_sites(ta_sites, template_positions)
+    #make complete dictionary of all possible ta sites including those with no insertions
     ta_dict = ta_sites_to_dict(ta_sites, res[0])
+    #write wig file
     wig_file = write_wig_from_dict(ta_dict, sample_name, genome)
+    
     return wig_file
